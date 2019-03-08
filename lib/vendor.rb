cd165d596dcf3d6235ed1f4a43e8b709c07fcd7e
Tatiana Quintana <tatsqui@gmail.com>
2019-03-07 21:36:05 -0800

    added tests for initiliaze, positive integer ids and keeping track of instantiated info in vendor

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,9 +1,25 @@
 module FarMar
 
   class Vendor
+    attr_reader :id, :name, :market_id, :num_of_employees
 
-    def initialize
+    def initialize(id:, name:, num_of_employees:, market_id:)
+      unless id.instance_of?(Integer) && id > 0
+        raise ArgumentError, "ID must be a positive number, given #{id}..."
+      end
 
+      unless market_id.instance_of?(Integer) && market_id > 0
+        raise ArgumentError, "Market ID must be a positive integer, give #{market_id}"
+      end
+
+      unless num_of_employees.instance_of?(Integer) && num_of_employees > 0
+        raise ArgumentError, "Number of employees must be a positive integer, given #{num_of_employees}..."
+      end
+
+      @id = id
+      @name = name
+      @num_of_employees = num_of_employees
+      @market_id = market_id
     end
 
   end
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -1,1 +1,69 @@
 require_relative 'spec_helper'
+
+describe "Vendor" do
+  describe "initialize" do
+
+    it "creates an instance of vendor" do
+      vendor = FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: 3, market_id: 1)
+      expect(vendor).must_be_kind_of FarMar::Vendor
+    end
+
+    it "keeps track of vendor id" do
+      id = 10
+      vendor = FarMar::Vendor.new(id: id, name: "Flying Lion Brewery", num_of_employees: 3, market_id: 1)
+      vendor.must_respond_to :id
+      vendor.id.must_equal id
+    end
+
+    it "requires vendor id to be a positive integer" do
+      proc {
+        FarMar::Vendor.new(id: -1, name: "Flying Lion Brewery", num_of_employees: 3, market_id: 1)
+      }.must_raise ArgumentError
+
+      proc {
+        FarMar::Vendor.new(id: "test", name: "Flying Lion Brewery", num_of_employees: 3, market_id: 1)
+      }
+    end
+
+    it "keeps track of vendor name" do
+      name = "test name"
+      vendor = FarMar::Vendor.new(id: 1, name: name, num_of_employees: 3, market_id: 1)
+      vendor.must_respond_to :name
+      vendor.name.must_equal name
+    end
+
+    it "keeps track of market_id" do
+      m_id = 33
+      vendor = FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: 3, market_id: m_id)
+      vendor.must_respond_to :market_id
+      vendor.market_id.must_equal m_id
+    end
+
+    it "requires market id to be a positive integer" do
+      proc {
+        FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: 3, market_id: "test")
+      }.must_raise ArgumentError
+
+      proc {
+        FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: 3, market_id: -11)
+      }.must_raise ArgumentError
+    end
+
+    it "keeps track of number of employees" do
+      num = 3
+      vendor = FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: num, market_id: 12)
+      vendor.must_respond_to :num_of_employees
+      vendor.num_of_employees.must_equal num
+    end
+
+    it "requires number of employees to be a positive integer" do
+      proc {
+        FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: -3, market_id: 6)
+      }.must_raise ArgumentError
+
+      proc {
+        FarMar::Vendor.new(id: 1, name: "Flying Lion Brewery", num_of_employees: "three", market_id: 4)
+      }.must_raise ArgumentError
+    end
+  end
+end
