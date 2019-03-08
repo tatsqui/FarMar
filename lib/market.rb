10745eea47e8f9bb059237d75cc506f685830224
Tatiana Quintana <tatsqui@gmail.com>
2019-03-07 20:04:12 -0800

    create instantiation tests for market_spec

--- lib/market.rb
+++ lib/market.rb
@@ -1,1 +1,17 @@
-TATIANA_CONSTANT = 30
+module FarMar
+
+  class Market
+    attr_reader :id
+
+    def initialize (id:, name:, address:, city:, county:, state:, zip:)
+      @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
+    end
+  end
+
+end
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,11 +1,33 @@
 require_relative 'spec_helper'
 
-describe "rake test" do
-  it "should run this test and it should fail" do
-    true.must_equal false
-  end
+describe "Market" do
+  describe "initialize" do
+    it "creates a new instance of market" do
+      market = FarMar::Market.new(
+                id: 1,
+                name: "People's Co-op Farmers Market",
+                address: "30th and Burnside",
+                city: "Portand",
+                county: "Multnomah",
+                state: "Oregon",
+                zip: "97282"
+              )
+      expect(market).must_be_kind_of FarMar::Market
+    end
 
-  it "should have access to stuff in market" do
-    expect(TATIANA_CONSTANT).must_equal 30
+    it "keeps track of id" do
+      id = 122
+      market = FarMar::Market.new(
+                id: id,
+                name: "People's Co-op Farmers Market",
+                address: "30th and Burnside",
+                city: "Portand",
+                county: "Multnomah",
+                state: "Oregon",
+                zip: "97282"
+              )
+      market.must_respond_to :id
+      market.id.must_equal id
+    end
   end
 end
